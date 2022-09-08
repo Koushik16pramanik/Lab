ef2d8c182c4b70de386fc3c3cf2de0fd2e01549e
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-08 21:47:07 +0530

    Add files via upload

---
+++ Labpart2.java
@@ -0,0 +1,10 @@
+package Lab;
+/*
+Develop a public java class and make sure nobody can create
+any object of this class from outside the class.
+ */
+
+class Labpart2{
+private Labpart2() {
+}
+	public static void main(String[] args) {  }}
