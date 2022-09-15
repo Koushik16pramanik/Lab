521b098dc2679668d523b933b52da8f45f242243
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-15 18:10:15 +0530

    Add files via upload

---
+++ 15.sept.lab/Inheritance.java
@@ -0,0 +1,12 @@
+package Lab;
+/*
+ Q.3… Class A serves as a base class for the derived class B, which in turn 
+serves as a base class for the derived class C. (Which type Of Inheritance 
+explain with an example)
+ */
+ class Inheritance {
+		}
+class C extends Inheritance{
+}
+class B extends C{
+}
