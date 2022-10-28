5eea4786030898608232faadb8599abbc743e319
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-10-28 18:20:30 +0530

    Update CheckOddEvenNumbers.java

--- 28.OCT.2022/CheckOddEvenNumbers.java
+++ 28.OCT.2022/CheckOddEvenNumbers.java
@@ -3,22 +3,17 @@ package Lab_seasons;
 /*  Problem 1. Create two threads to print even numbers and odd numbers from 1 to 100?
 note: after printing of all even numbers only odd numbers should print   */
 
-public class CheckOddEvenNumbers {  // main method
-		public static void main(String[] args) {
-			Runnable r1 = new Runnable1();
-			Runnable r2 = new Runnable2();
-			Thread t1 = new Thread(r1);
-			Thread t2 = new Thread(r2);
-			t1.start();
-			t2.start();
-		}}
-	class Runnable1 implements Runnable {   // implement class1
-		public void run() {
-			for (int i = 1; i <= 11; i += 2) {
-				System.out.println(i);
-			}}}
-	class Runnable2 implements Runnable {   // implement class2
-		public void run() {
-			for (int i = 0; i < 11; i += 2) {
-				System.out.println(i);
-			}}}
+public class CheckOddEvenNumbers { //Main Class
+	public static void main(String[] args) throws Exception { //Main Method
+		System.out.println("Even: ");
+		for(int i=0; i<=100; i++ ) {
+			if(i%2 == 0) { //Check even or not 
+				System.out.println(i + ","); 
+			}}
+		Thread.sleep(1000);
+		System.out.println();
+		System.out.println("Odd: ");
+		for(int i=0; i<=100; i++ ) {
+			if(i%2 != 0) { //Check Odd or not
+				System.out.println(i + ","); 
+			}}}}
