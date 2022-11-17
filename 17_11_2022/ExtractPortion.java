eebcd6bc41dc96cb43a2dd9cd2f9b5811c1ecedc
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-11-17 17:35:22 +0530

    Update ExtractPortion.java

--- 17_11_2022/ExtractPortion.java
+++ 17_11_2022/ExtractPortion.java
@@ -1,19 +1,18 @@
 package Lab_seasons;
 import java.util.*;
 /*   Problem Statement 3: 
-  
   Write a Java program to extract a portion of an array list.     */
 
 public class ExtractPortion {
-		public static void main(String[] args) {
-		ArrayList<String> al = new ArrayList<String>();
+		public static void main(String[] args) {  //main method
+		ArrayList<String> al = new ArrayList<String>();   // Creating array list
 		al.add("Red");
 		al.add("Green");
 		al.add("Orange");
 		al.add("White");
 		al.add("Black");
 			
-		System.out.println("Array List" + al);
-		List<String> al1= al.subList(0, 3);
+		System.out.println("Array List" + al);   
+		List<String> al1= al.subList(0, 3);   // extract element
 		System.out.println("extract a portion of an array list "+ al1);
 	}}
