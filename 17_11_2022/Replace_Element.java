490b5e29f652c6ec5e261dad7c68cb36c9331ad1
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-11-17 18:31:58 +0530

    Add files via upload

---
+++ 17_11_2022/Replace_Element.java
@@ -0,0 +1,16 @@
+package Lab_seasons;
+import java.util.*;
+/*    Problem Statement 5:
+ Write a Java program to replace an element in a linked list.   */
+
+public class Replace_Element {
+			public static void main(String[] args) {
+			LinkedList<String> all = new LinkedList<String>();
+			all.add("Rose");
+			all.add("Lavender");
+			all.add("Lotas");
+			all.add("Jasmine");
+		    System.out.println("The List is: " + all);
+		    all.set(2, "Sunflower");
+		    System.out.println("The List is: " + all);
+			}}
